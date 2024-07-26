952f95410a6b5c4555a99736d2dba9d79f9772bb
Daria Daleka <sovyshkamy@gmail.com>
2024-07-26 20:28:23 +0200

    add changes 1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,25 @@
 export default function searchImagesByQuery(query) {
   const URL = 'https://pixabay.com/api/';
   const API_KEY = '45132179-4e43907cb4a483db7f713893c';
+  return fetch(
+    `${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+  )
+    .then(response => {
+      console.log(`Received response:`, response);
+      if (!response.ok) {
+        throw new Error(response.statusText || 'Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Data received:', data);
+      return data;
+    })
+    .catch(error => {
+      console.error('Error fetching images:', error);
+      iziToast.error({
+        position: 'topRight',
+        message: `Error: ${error.message}`,
+      });
+    });
 }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.search-form');
+const input = document.querySelector('.gallery-search-input');
+const loader = document.querySelector('.loader');
+
+form.addEventListener('submit', handleSubmit);
